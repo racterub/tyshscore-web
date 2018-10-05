031480ab7d3f105be82100287bb33d8af8224e15
racterub <vivi.450@hotmail.com>
2018-10-06 00:00:09 +0800

    remove debug feature

--- website/static/js/check.js
+++ website/static/js/check.js
@@ -1,11 +1,7 @@
 function check() {
 	var stdid = document.getElementById("stdid");
 	var passwd = document.getElementById("stdpwd");
-	console.log(stdid.checkValidity())
-	console.log(stdpwd.checkValidity())
 	if (stdid.checkValidity() == true && stdpwd.checkValidity() == true) {
-		console.log(stdid);
-		console.log(stdpwd);
 		document.getElementById('loading').style = '';
 	}
 }
