8d1c8c70f30fe1a41b3e5e3f105cced2bf5c921e
racterub <vivi.450@hotmail.com>
2018-11-21 00:13:46 +0800

    make it easier to change from prod to dev

--- serve.py
+++ serve.py
@@ -7,11 +7,18 @@
 
 from website import app
 import os
+import sys
 
 if __name__ == '__main__':
-    if app.config['DEBUG']:
-        port = 6969
-    else:
+    if len(sys.argv) == 1:
         port = int(os.environ.get("PORT", 5000))
+    else:
+        if sys.argv[1] == 'dev':
+            print('Running in Debug mode')
+            app.config['DEBUG'] = True
+            port = 6969
+        else:
+            print('Usage: ./%s (dev)' % sys.argv[0])
+            sys.exit(1)
     app.run(host='0.0.0.0', port=port, threaded=True)
 
