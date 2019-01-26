3b8036efe8bd34f556e2c6f62b5b5382532496be
racterub <vivi.450@hotmail.com>
2019-01-26 21:39:20 +0800

    add parent mode

--- website/views/main.py
+++ website/views/main.py
@@ -10,24 +10,31 @@ from lxml import etree
 import requests
 from website.views.lib.crawler import login, get_score
 
+#Initial globs
 uid = u''
+parent_mode=False
 content = []
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     global uid
     global content
+    global parent_mode
     if request.method == "POST":
         if request.form:
             stdid = request.form['stdid']
             stdpwd = request.form['stdpwd']
             status = login(stdid, stdpwd)
             if status == True:
+                if stdid[-1] == "p":
+                    stdid = stdid[:-1]
+                    parent_mode = True
                 uid = request.form['stdid']
                 session['user'] = request.form['stdid']
                 content = get_score()
                 flash(u"登入成功")
-                return render_template('index.html', stdid=uid)
+                flash(u"將使用家長模式登入")
+                return render_template('index.html', stdid=uid, parent_mode=parent_mode)
             else:
                 info = u"帳號密碼錯誤，請再次確認"
                 return render_template('index.html', info=info)
@@ -45,28 +52,29 @@ def index():
             session.pop('logout', None)
             return render_template('index.html', info=info)
         else:
-            return render_template('index.html', stdid=uid)
+            return render_template('index.html', stdid=uid, parent_mode=parent_mode)
 
 @app.route('/scoreboard/<int:counter>')
 def scoreboard(counter):
     global uid
     global content
+    global parent_mode
     if 'user' in session:
         if (counter <= 0 | counter > 3): #Also Harcoded..
             error_header = u"資料無法處理"
             error_context = u"您所選的資料目前無法處理或是校方系統資料已清空，請稍後再試"
-            return render_template('error.html', stdid=uid, error_header=error_header, error_context=error_context), 400
+            return render_template('error.html', stdid=uid, error_header=error_header, error_context=error_context, parent_mode=parent_mode), 400
         if content:
             pass
         else:
             content = get_score()
         if content == False:
-            return render_template('error.html', stdid=uid)
+            return render_template('error.html', stdid=uid, parent_mode=parent_mode)
         body = []
         for i in content:
             body.append(i[counter-1])
         #Hardcoded table header :(
-        return render_template('scoreboard.html', head=[u'科目', u'成績', u'全班平均', u'班級排名', u'班級人數'], body=body, stdid=uid, count=counter)
+        return render_template('scoreboard.html', head=[u'科目', u'成績', u'全班平均', u'班級排名', u'班級人數'], body=body, stdid=uid, count=counter, parent_mode=parent_mode)
 
     else:
         session['redirect'] = u'請先登入系統'
