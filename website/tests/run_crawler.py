690ae3085da06e62828141009718ecedad4fe8fc
racterub <vivi.450@hotmail.com>
2019-02-10 23:16:55 +0800

    flush

--- website/tests/run_crawler.py
+++ website/tests/run_crawler.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from ..views.lib.crawler import logi
+from ..views.lib.crawler import login, get_history_pr, get_term_score
 
 username = ''
 password = ''
@@ -8,6 +8,8 @@ password = ''
 login(username, password)
 
 #print(beta_get_score()) #Issue mentioned in crawler module
-print(beta_bs4_score())
+# print(beta_bs4_score())
 # print(get_score())
 
+print(get_history_pr())
+# print(get_term_score())
